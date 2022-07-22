fa974d83b2ddaac9eaa5cd1e3b631c053fa61a60
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-07-22 22:32:46 +0530

    Welcome message displayed

---
+++ src/com/bridgelabz/wages/EmployeeWages.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.wages;
+
+public class EmployeeWages {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage computation program");
+    }
+}
